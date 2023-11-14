d59c20a1ac9b0575cc378b461db31d3e634156b5
Jana Aloufi <Jana.1rrt@gmail.com>
2023-11-15 01:41:50 +0300

    changes

--- SensorDataProcessor.java
+++ SensorDataProcessor.java
@@ -26,28 +26,28 @@ public class SensorDataProcessor {
 
         int i, j, k = 0;
         double[][][] data2 = new
-    double[data.length][data[0].length][data[0][0].length];
+    double[sensorData.length][sensorData[0].length][sensorData[0][0].length];
         BufferedWriter fileWriter;
 
     // Write racing stats data into a file
         try {
             fileWriter = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
             
-            for (i = 0; i < data.length; i++) {
-                for (j = 0; j < data[0].length; j++) {
-                    for (k = 0; k < data[0][0].length; k++) {
-                        data2[i][j][k] = data[i][j][k] / d -
+            for (i = 0; i < sensorData.length; i++) {
+                for (j = 0; j < sensorData[0].length; j++) {
+                    for (k = 0; k < sensorData[0][0].length; k++) {
+                        data2[i][j][k] = sensorData[i][j][k] / d -
 Math.pow(limit[i][j], 2.0);
 
                         if (calculateAverage(data2[i][j]) > 10 && calculateAverage(data2[i][j]) 
 < 50)
                             break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) > 
-data[i][j][k])
+                        else if (Math.max(sensorData[i][j][k], data2[i][j][k]) > 
+sensorData[i][j][k])
                             break;
                         else if (Math.pow(Math.abs(data[i][j][k]), 3) < 
 Math.pow(Math.abs(data2[i][j][k]), 3)
-                            && calculateAverage(data[i][j]) < data2[i][j][k] && (i + 1) 
+                            && calculateAverage(sensorData[i][j]) < data2[i][j][k] && (i + 1) 
 * (j + 1) > 0)
                             data2[i][j][k] *= 2;
                         else
