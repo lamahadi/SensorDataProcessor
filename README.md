38ec91630adc211d979543ccd72856c9f1c50173
Rasho22rr <146008714+Rasho22rr@users.noreply.github.com>
2023-11-26 19:30:47 +0300

    Add files via upload
    The code is devoid of appropriate comments and documentation outlining its functions, goals, and usage. To improve the readability and maintainability of the code, I've included comments and documentation.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Assignment#2 CCSW 325 Software Construction
 # Student Name: Jana Aloufi   Student ID: 2111535, Change the naming of variables to be more descriptive and readable. The name if constructor not match with name of class so now it fixed.
 
+# Student name:Rasha Alyazydi Student ID:2114819
--- SensorDataProcessor.java
+++ SensorDataProcessor.java
@@ -1,32 +1,46 @@
+/**
+ * The SensorDataProcessor class processes sensor data and writes the calculated data to a file.
+ */
 public class SensorDataProcessor {
 
     // Senson data and limits.
     public double[][][] sensorData;
     public double[][] limit;
 
-    // constructor
+    /**
+     * Constructor for SensorDataProcessor.
+     *
+     * @param sensorData The sensor data.
+     * @param limit The limits.
+     */
     public SensorDataProcessor(double[][][] sensorData, double[][] limit) {
         this.sensorData = sensorData;
         this.limit = limit;
     }
 
-    // calculates average of sensor data
+    /**
+     * Calculates the average of an array.
+     *
+     * @param array The input array.
+     * @return The average of the array.
+     */
     private double calculateAverage(double[] array) {
         int i = 0;
         double sum = 0;
         for (i = 0; i < array.length; i++) {
             sum += array[i];
         }
-
         return sum / array.length;
     }
 
-    // calculate data
+    /**
+     * Calculates the data and writes it to a file.
+     *
+     * @param d The value 'd' used in the calculation.
+     */
     public void calculateData(double d) {
-
         int i, j, k = 0;
-        double[][][] data2 = new
-    double[sensorData.length][sensorData[0].length][sensorData[0][0].length];
+        double[][][] data2 = new double[sensorData.length][sensorData[0].length][sensorData[0][0].length];
         BufferedWriter fileWriter;
 
     // Write racing stats data into a file
@@ -36,19 +50,15 @@ public class SensorDataProcessor {
             for (i = 0; i < sensorData.length; i++) {
                 for (j = 0; j < sensorData[0].length; j++) {
                     for (k = 0; k < sensorData[0][0].length; k++) {
-                        data2[i][j][k] = sensorData[i][j][k] / d -
-Math.pow(limit[i][j], 2.0);
+                        data2[i][j][k] = sensorData[i][j][k] / d - Math.pow(limit[i][j], 2.0);
 
-                        if (calculateAverage(data2[i][j]) > 10 && calculateAverage(data2[i][j]) 
-< 50)
+                        // Check conditions and perform calculations
+                        if (calculateAverage(data2[i][j]) > 10 && calculateAverage(data2[i][j]) < 50)
                             break;
-                        else if (Math.max(sensorData[i][j][k], data2[i][j][k]) > 
-sensorData[i][j][k])
+                        else if (Math.max(sensorData[i][j][k], data2[i][j][k]) > sensorData[i][j][k])
                             break;
-                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < 
-Math.pow(Math.abs(data2[i][j][k]), 3)
-                            && calculateAverage(sensorData[i][j]) < data2[i][j][k] && (i + 1) 
-* (j + 1) > 0)
+                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+                            && calculateAverage(sensorData[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
                             data2[i][j][k] *= 2;
                         else
                         continue;
@@ -56,6 +66,7 @@ Math.pow(Math.abs(data2[i][j][k]), 3)
                 }
             }
 
+            // Write calculated data to the file
             for (i = 0; i < data2.length; i++) {
                 for (j = 0; j < data2[0].length; j++) {
                     fileWriter.write(data2[i][j] + "\t");
@@ -68,5 +79,4 @@ Math.pow(Math.abs(data2[i][j][k]), 3)
             System.out.println("Error= " + e);
         }
     }
-    
 }
